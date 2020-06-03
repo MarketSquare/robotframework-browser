4cc6de19a3218a22299052a249395a10f9d771cf
Mikko Korpela <mikko.korpela@gmail.com>
2020-06-03 23:20:55 +0300

    client calls

--- Playwright/__init__.py
+++ Playwright/__init__.py
@@ -1,9 +1,18 @@
 __version__ = "0.1.0"
+import grpc  # type: ignore
+
+import Playwright.generated.playwright_pb2_grpc as playwright_pb2_grpc
 
 
 class Playwright:
-    def open_browser(self):
-        pass
+    @staticmethod
+    def open_browser():
+        with grpc.insecure_channel("localhost:50051") as channel:
+            stub = playwright_pb2_grpc.PlaywrightStub(channel)
+            stub.OpenBrowser()
 
-    def close_browser(self):
-        pass
+    @staticmethod
+    def close_browser():
+        with grpc.insecure_channel("localhost:50051") as channel:
+            stub = playwright_pb2_grpc.PlaywrightStub(channel)
+            stub.CloseBrowser()
