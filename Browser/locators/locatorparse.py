148c695157d17a02ce0e48636d5a91949241c1ff
Tatu Aalto <aalto.tatu@gmail.com>
2020-06-17 00:37:00 +0300

    Support for ligth operator

--- Browser/locators/locatorparse.py
+++ Browser/locators/locatorparse.py
@@ -1,11 +1,20 @@
 class LocatorParse:
 
-    _strategies = ["css", "xpath", "id", "text"]
+    _strategies_light = ("css:light", "text:light")
+    _strategies = ("css", "xpath", "id", "text", *_strategies_light)
 
     def parse_locator(self, locator: str):
         if locator.startswith(("//", "(//")):
             return f"xpath={locator}"
-        if ":" not in locator:
-            return f"css={locator}"
-        strategy, locator = locator.split(":", 1)
-        return f"{strategy.rstrip()}={locator.lstrip()}"
+        index = self._strategy_index(locator)
+        if index != -1:
+            strategy = locator[:index].strip()
+            if strategy in self._strategies:
+                return f"{strategy}={locator[index + 1:].lstrip()}"
+        return f"css={locator}"
+
+    def _strategy_index(self, locator: str):
+        if locator.lower().startswith(self._strategies_light):
+            strategy = ":".join(locator.split(":", 2)[:2])
+            return len(strategy)
+        return locator.find(":")
