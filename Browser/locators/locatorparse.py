5cc2d7e70960af6ea2d594f920a951a1231d8eee
Tatu Aalto <aalto.tatu@gmail.com>
2020-06-16 23:57:15 +0300

    Text support

--- Browser/locators/locatorparse.py
+++ Browser/locators/locatorparse.py
@@ -1,13 +1,10 @@
 class LocatorParse:
 
-    _strategies = ["css", "xpath", "id"]
-    css = "css"
-    xpath = "xpath"
-    id_ = "id"
+    _strategies = ["css", "xpath", "id", "text"]
 
     def parse_locator(self, locator: str):
         if locator.startswith(("//", "(//")):
-            return f"{self.xpath}={locator}"
+            return f"xpath={locator}"
         if ":" not in locator:
             return f"css={locator}"
         strategy, locator = locator.split(":", 1)
--- utest/test_locator_parse.py
+++ utest/test_locator_parse.py
@@ -18,6 +18,12 @@ def test_parse_xpath(parse: LocatorParse):
     assert parse.parse_locator("//div") == "xpath=//div"
     assert parse.parse_locator("(//div)[0]") == "xpath=(//div)[0]"
     assert parse.parse_locator("xpath://div") == "xpath=//div"
+    assert parse.parse_locator('//div[text="some:text"]') == 'xpath=//div[text="some:text"]'
+
+
+def test_parse_text(parse: LocatorParse):
+    assert parse.parse_locator('text:"foobar"') == 'text="foobar"'
+    assert parse.parse_locator(r'text:/^\\s*Login$/i') == r'text=/^\\s*Login$/i'
 
 
 def test_parse_id(parse: LocatorParse):
