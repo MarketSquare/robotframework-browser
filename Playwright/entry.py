dcc5e46ac344aa5d2b271cfc2dab3721b27e6e94
Janne Härkönen <jth@reaktor.com>
2020-06-16 14:05:40 +0300

    Introduce setup.py to use setuptools for building
    It seems that our target of packaging js files inside out installed
    Pythton module is not very easy to achieve with Poetry, but straight
    setuptools works well enough.
    
    The problem with poetry is that it does not allow using files lsited in
    .gitignore in `package_data`, so e.g. compiled js files do not end up
    in the sdist generated by poetry

---
+++ Playwright/entry.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from subprocess import Popen, PIPE
+
+USAGE = """USAGE
+  robotframework-playwright [command]
+
+AVAILABLE COMMANDS
+  init  Install required nodejs dependencies
+"""
+
+
+def run():
+    if len(sys.argv) < 2:
+        print(USAGE)
+        sys.exit(1)
+    cmd = sys.argv[1]
+    if cmd == "init":
+        print("installing node dependencies...")
+        installation_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "wrapper"
+        )
+        process = Popen(
+            "npm install", shell=True, cwd=installation_dir, stdout=PIPE, stderr=PIPE,
+        )
+        if process.stdout is None:
+            raise RuntimeError("problem installing node dependencies")
+        for line in process.stdout:
+            print(line.decode("utf-8"))
+    print(f"Invalid command `{cmd}`")
+    print(USAGE)
---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup  # type: ignore
+
+packages = \
+['Playwright', 'Playwright.generated', 'Playwright.locators']
+
+package_data = \
+{'': ['*'], 'Playwright': ['wrapper/*', 'wrapper/generated/*']}
+
+install_requires = \
+['grpcio-tools>=1.29.0,<2.0.0', 'grpcio>=1.29.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'robotframework-playwright',
+    'version': '0.1.0',
+    'description': '',
+    'author': 'Mikko Korpela',
+    'author_email': 'mikko.korpela@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mkorpela/robotframework-playwright',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': {'console_scripts': ['robotframework-playwright=Playwright.entry:run']},
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
