ef40e3d66303edfa190ed0324d683e8aec6404e2
Tatu Aalto <aalto.tatu@gmail.com>
2020-06-18 22:37:35 +0300

    Support for multipart locator

--- Browser/locators/locatorparse.py
+++ Browser/locators/locatorparse.py
@@ -1,9 +1,35 @@
+import re
+from typing import List
+
+
 class LocatorParse:
 
     _strategies_light = ("css:light", "text:light")
     _strategies = ("css", "xpath", "id", "text", *_strategies_light)
 
-    def parse_locator(self, locator: str):
+    def parse_locator(self, locator: str) -> str:
+        full_locator = ""
+        for part in self._split_locator(locator):
+            if part != " >> ":
+                part = self._parse_locator(part)
+            full_locator = f"{full_locator}{part}"
+        return full_locator
+
+    def _split_locator(self, locator: str) -> List[str]:
+        locator_parts = []
+        while True:
+            parts = re.search(r"(.*)(\s>>\s)(xpath:|css:|id:|text:)(.*)", locator)
+            if parts:
+                locator = parts.group(1)
+                locator_parts.append(f"{parts.group(3)}{parts.group(4)}")
+                locator_parts.append(parts.group(2))
+            if not parts:
+                locator_parts.append(locator)
+                break
+        locator_parts.reverse()
+        return locator_parts
+
+    def _parse_locator(self, locator: str) -> str:
         if locator.startswith(("//", "(//")):
             return f"xpath={locator}"
         index = self._strategy_index(locator)
@@ -13,7 +39,7 @@ class LocatorParse:
                 return f"{strategy}={locator[index + 1:].lstrip()}"
         return f"css={locator}"
 
-    def _strategy_index(self, locator: str):
+    def _strategy_index(self, locator: str) -> int:
         if locator.lower().startswith(self._strategies_light):
             strategy = ":".join(locator.split(":", 2)[:2])
             return len(strategy)
--- utest/test_locator_parse.py
+++ utest/test_locator_parse.py
@@ -19,12 +19,15 @@ def test_parse_xpath(parse: LocatorParse):
     assert parse.parse_locator("//div") == "xpath=//div"
     assert parse.parse_locator("(//div)[0]") == "xpath=(//div)[0]"
     assert parse.parse_locator("xpath://div") == "xpath=//div"
-    assert parse.parse_locator('//div[text="some:text"]') == 'xpath=//div[text="some:text"]'
+    assert (
+        parse.parse_locator('//div[text="some:text"]')
+        == 'xpath=//div[text="some:text"]'
+    )
 
 
 def test_parse_text(parse: LocatorParse):
     assert parse.parse_locator('text:"foobar"') == 'text="foobar"'
-    assert parse.parse_locator(r'text:/^\\s*Login$/i') == r'text=/^\\s*Login$/i'
+    assert parse.parse_locator(r"text:/^\\s*Login$/i") == r"text=/^\\s*Login$/i"
 
 
 def test_parse_id(parse: LocatorParse):
@@ -32,6 +35,34 @@ def test_parse_id(parse: LocatorParse):
 
 
 def test_strategy_index(parse: LocatorParse):
-    assert parse._strategy_index('id:password') == 2
-    assert parse._strategy_index('password') == -1
-    assert parse._strategy_index('css:light:div') == 9
+    assert parse._strategy_index("id:password") == 2
+    assert parse._strategy_index("password") == -1
+    assert parse._strategy_index("css:light:div") == 9
+
+
+def test_chained_locators(parse: LocatorParse):
+    result = parse.parse_locator("xpath://html/body/div >> css:span")
+    assert result == "xpath=//html/body/div >> css=span"
+    result = parse.parse_locator('xpath://div[@text=" >> "]')
+    assert result == 'xpath=//div[@text=" >> "]'
+    result = parse.parse_locator('xpath://div[@text=">>"] >> xpath://div[@text=">>"]')
+    assert result == 'xpath=//div[@text=">>"] >> xpath=//div[@text=">>"]'
+    result = parse.parse_locator("xpath://html/body/div>>css:span")
+    assert result == "xpath=//html/body/div>>css:span"
+
+
+def test_split_locator(parse: LocatorParse):
+    assert parse._split_locator("css:span") == ["css:span"]
+    assert parse._split_locator("xpath://html/body/div >> css:span") == [
+        "xpath://html/body/div",
+        " >> ",
+        "css:span",
+    ]
+    assert parse._split_locator('xpath://div[@text=" >> "]') == [
+        'xpath://div[@text=" >> "]'
+    ]
+    assert parse._split_locator('text:" >> " >> text: " >> "') == [
+        'text:" >> "',
+        " >> ",
+        'text: " >> "',
+    ]
