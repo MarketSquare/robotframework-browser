88715ce4596b630e5dbcb362ee088668997493a0
Mikko Korpela <mikko.korpela@gmail.com>
2020-06-25 09:09:41 +0300

    Bump version 0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'robotframework-browser',
-    'version': '0.1.1',
+    'version': '0.2.0',
     'description': '',
     'long_description': long_description,
     'long_description_content_type': 'text/markdown',
