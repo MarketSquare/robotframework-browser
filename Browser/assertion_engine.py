e1fd9feb335ef03d6a1376598d78c025c6017ee5
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-07-01 14:51:04 +0300

    Use enums in assertion engine mapping, add type ignores to work around mypy #9079

--- Browser/assertion_engine.py
+++ Browser/assertion_engine.py
@@ -2,84 +2,72 @@ from enum import Enum
 from typing import Any
 import re
 
-AssertionOperator = Enum(
-    "AssertionOperator",
-    "NO_ASSERTION == should_be != should_not_be < > <= >= *= contains matches ^= $="
+AssertionOperator = Enum(  # type: ignore
+    value="AssertionOperator",
+    names={
+        "noassertion": None,
+        "NO_ASSERTION": None,
+        "equal": "==",
+        "==": "==",
+        "shouldbe": "==",
+        "inequal": "!=",
+        "!=": "!=",
+        "shouldnotbe": "!=",
+        "lessthan": "<",
+        "<": "<",
+        "greaterthan": ">",
+        ">": ">",
+        "<=": "<=",
+        ">=": ">=",
+        "contains": "*=",
+        "*=": "*=",
+        "starts": "^=",
+        "^=": "^=",
+        "shouldstartwith": "^=",
+        "ends": "$=",
+        "$=": "$=",
+        "matches": "$",
+    },
 )
 
 
 def _normalize(operator: str):
-    return operator.lower().replace('_', '').replace(' ', '')
+    return operator.lower().replace("_", "").replace(" ", "")
 
 
-def verify_assertion(value: Any,
-                     operator: AssertionOperator,
-                     expected, message=""):
-
+def verify_assertion(value: Any, operator: AssertionOperator, expected, message=""):
     if isinstance(operator, str):
-        if _normalize(operator) == "contains":
-            operator = AssertionOperator["*="]
-        elif _normalize(operator) == "shouldcontain":
-            operator = AssertionOperator["*="]
-        elif _normalize(operator) == "matches":
-            operator = AssertionOperator["matches"]
-        elif _normalize(operator) == "shouldbe":
-            operator = AssertionOperator["=="]
-        elif _normalize(operator) == "shouldnotbe":
-            operator = AssertionOperator["!="]
-        elif _normalize(operator) == "shouldstartwith":
-            operator = AssertionOperator["^="]
-        elif _normalize(operator) == "shouldendwith":
-            operator = AssertionOperator["$="]
-        else:
-            raise SyntaxError(f"{message}"
-                              f" `{operator}` is no valid assertion operator")
+        operator = _normalize(operator)
+        try:
+            operator = AssertionOperator[operator]  # type: ignore
+        except NameError:
+            raise RuntimeError(
+                f"{message}" f" `{operator}` is not a valid assertion operator."
+            )
 
-    if operator.name in ["==", "should_be"] and not value == expected:
-        raise AssertionError(
-            f"{message} `{value}` should be `{expected}`"
-        )
-    elif operator.name in ["!=", "should_not_be"] and not value != expected:
-        raise AssertionError(
-            f"{message} `{value}` should not be `{expected}`"
-        )
-    elif operator.name == "<" and not value < expected:
-        raise AssertionError(
-            f"{message} `{value}` should be less than `{expected}`"
-        )
-    elif operator.name == ">" and not value > expected:
-        raise AssertionError(
-            f"{message} `{value}` should be greater than `{expected}`"
-        )
-    elif operator.name == "<=" and not value <= expected:
+    if operator.value == "==" and not value == expected:
+        raise AssertionError(f"{message} `{value}` should be `{expected}`")
+    elif operator.value == "!=" and not value != expected:
+        raise AssertionError(f"{message} `{value}` should not be `{expected}`")
+    elif operator.value == "<" and not value < expected:
+        raise AssertionError(f"{message} `{value}` should be less than `{expected}`")
+    elif operator.value == ">" and not value > expected:
+        raise AssertionError(f"{message} `{value}` should be greater than `{expected}`")
+    elif operator.value == "<=" and not value <= expected:
         raise AssertionError(
             f"{message} `{value}` should be less than or equal `{expected}`"
         )
-    elif operator.name == ">=" and not value >= expected:
+    elif operator.value == ">=" and not value >= expected:
         raise AssertionError(
             f"{message} `{value}` should be greater than or equal `{expected}`"
         )
-    elif operator.name in ["contains", "*="] and expected not in value:
-        raise AssertionError(
-            f"{message} `{value}` should contain `{expected}`"
-        )
-    elif operator.name == "matches" and not re.search(expected, value):
-        raise AssertionError(
-            f"{message} `{value}` should match `{expected}`"
-        )
-    elif operator.name == "^=" and not re.search(
-            f"^{re.escape(expected)}", value
-    ):
-        raise AssertionError(
-            f"{message} `{value}` should start with `{expected}`"
-        )
-    elif operator.name == "$=" and not re.search(
-            f"{re.escape(expected)}$", value
-    ):
-        raise AssertionError(
-            f"{message} `{value}` should end with `{expected}`"
-        )
-    elif operator.name not in AssertionOperator.__members__:
-        raise SyntaxError(
-            f"{message} `{operator.name}` is not a valid assertion operator"
-        )
+    elif operator.value == "*=" and expected not in value:
+        raise AssertionError(f"{message} `{value}` should contain `{expected}`")
+    elif operator.value == "$" and not re.search(expected, value):
+        raise AssertionError(f"{message} `{value}` should match `{expected}`")
+    elif operator.value == "^=" and not re.search(f"^{re.escape(expected)}", value):
+        raise AssertionError(f"{message} `{value}` should start with `{expected}`")
+    elif operator.value == "$=" and not re.search(f"{re.escape(expected)}$", value):
+        raise AssertionError(f"{message} `{value}` should end with `{expected}`")
+    elif operator not in AssertionOperator:
+        raise RuntimeError(f"{message} `{operator}` is not a valid assertion operator")
