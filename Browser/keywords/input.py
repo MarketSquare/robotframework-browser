0832a7270fa38a92cf4519efa55c0bd9d42b8ed1
Mikko Korpela <mikko.korpela@gmail.com>
2020-07-03 08:57:40 +0300

    no deprecated keywords at this point

--- Browser/keywords/input.py
+++ Browser/keywords/input.py
@@ -18,21 +18,6 @@ class Input:
         return self.library.playwright
 
     @keyword
-    def input_text(self, selector: str, text: str, type=False):
-        """ DEPRECATED Inputs the given ``text`` into the text field identified by ``selector``
-
-            By default text is inputted via filling (instantly), only triggering the
-            input event. By toggling the ``type`` boolean text will be typed into the
-            field instead. Typing triggers keydown, keypress/input and keyup events
-            for every character of input.
-        """
-        with self.playwright.grpc_channel() as stub:
-            response = stub.InputText(
-                Request().inputText(input=text, selector=selector, type=type)
-            )
-            logger.info(response.log)
-
-    @keyword
     def type_text(
         self, selector: str, text: str, delay: str = "0 ms", clear: bool = True
     ):
@@ -110,29 +95,6 @@ class Input:
             self.fill_text(selector, secret)
         finally:
             BuiltIn().set_log_level(previous_level)
-
-    @keyword
-    def input_password(self, selector: str, password: str):
-        """ DEPRECATED Types the given ``password`` into the field identified by ``selector``
-            Disables logging to avoid leaking sensitive information.
-        Difference compared to `Input Text` is that this keyword does not
-        log the given password on the INFO level. Notice that if you use
-        the keyword like
-        | Input Password | password_field | password |
-        the password is shown as a normal keyword argument. A way to avoid
-        that is using variables like
-        | Input Password | password_field | ${PASSWORD} |
-        Please notice that Robot Framework logs all arguments using
-        the TRACE level and tests must not be executed using level below
-        DEBUG if the password should not be logged in any format.
-        """
-        with self.playwright.grpc_channel() as stub:
-            try:
-                # Should prevent logging in case of failure keywords
-                previous_level = BuiltIn().set_log_level("NONE")
-                stub.InputText(Request().inputText(input=password, selector=selector))
-            finally:
-                BuiltIn().set_log_level(previous_level)
 
     @keyword
     def press_keys(self, selector: str, *keys: str):
