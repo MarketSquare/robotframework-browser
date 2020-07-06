e2d2eb0aedb4ea9b403a49a247e2a18b386c180e
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-07-06 10:23:30 +0300

    Add webpack, create dynamic-test-app folder
    Add webpack html plugin
    
    Add functionality to dynamic demo app

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './dynamic-test-app/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './dynamic-test-app/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'atest/demoapp/html/dist/')
+  },
+};
