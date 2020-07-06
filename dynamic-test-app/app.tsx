e8876b662f5334277fbc04a5b24e28a9a6b6f489
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-07-06 10:23:30 +0300

    Add webpack, create dynamic-test-app folder
    Add webpack html plugin
    
    Add functionality to dynamic demo app

---
+++ dynamic-test-app/app.tsx
@@ -0,0 +1,64 @@
+import React, { ChangeEvent, useState, useRef } from "react";
+
+export default function Site() {
+    const username = useRef("");
+    const password  = useRef("");
+    const [submit, setSubmit] = useState(false)
+
+    const handleSubmit = () => {
+        setSubmit(true)
+    }
+
+    function usernameChange(event: ChangeEvent<HTMLInputElement>) {
+        username.current = event.target.value
+    }
+    function passwordChange(event: ChangeEvent<HTMLInputElement>) {
+        password.current = event.target.value
+    }
+
+    function Body() {
+        if(submit) return <PostSubmit/>
+        else return <PreSubmit />
+    }
+
+    function PreSubmit() {
+        return <>
+        <h1>Login Page</h1>
+        <p>Please input your user name and password and click the login button.</p>
+        <form name="login_form" onSubmit={handleSubmit} >
+            <table>
+                <tr>
+                    <td><label htmlFor="username_field">User Name:</label></td>
+                    <td><input id="username_field" size={30} type="text" onChange={usernameChange} /></td>
+                </tr>
+                <tr>
+                    <td><label htmlFor="password_field">Password:</label></td>
+                    <td><input id="password_field" size={30} type="password" onChange={passwordChange} /></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><input id="login_button" type="submit" value="LOGIN" /></td>
+                </tr>
+            </table>
+        </form>
+    </>
+    }
+
+    function PostSubmit() {
+        if (username.current == "demo" && password.current == "mode") {
+            document.title = "Welcome Page"
+            return (<>
+                <h1>Welcome Page</h1>
+                <p>Login succeeded. Now you can <a href=".">logout</a>.</p>
+            </>)
+        } else {
+            document.title = "Error Page"
+            return (<>    
+                <h1>Error Page</h1>
+                <p>Login failed. Invalid user name and/or password.</p>
+            </>) 
+        }
+    }
+
+    return <Body />
+}
