a06f7ee7da52dc20161233e6b559b3d560955f62
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-07-07 12:37:38 +0300

    Add Wait For Element's State test

--- atest/dynamic-test-app/src/app.tsx
+++ atest/dynamic-test-app/src/app.tsx
@@ -1,5 +1,15 @@
 import React, { ChangeEvent, useState, useRef } from "react";
 
+function goesInvisible(event: React.MouseEvent<HTMLButtonElement>) {
+    event.persist()
+    console.log("Going hidden in 1 second")
+    setTimeout(() => {
+        //@ts-expect-error
+        event.target.style.visibility = "hidden";
+        console.log("Went hidden")
+    }, 500)
+}
+
 export default function Site() {
     const username = useRef("");
     const password  = useRef("");
@@ -26,24 +36,25 @@ export default function Site() {
         return <>
         <h1>Login Page</h1>
         <p>Please input your user name and password and click the login button.</p>
-        <form name="login_form" onSubmit={handleSubmit} >
-            <table>
-                <tbody>
-                <tr>
-                    <td><label htmlFor="username_field">User Name:</label></td>
-                    <td><input id="username_field" size={30} type="text" onChange={usernameChange} /></td>
-                </tr>
-                <tr>
-                    <td><label htmlFor="password_field">Password:</label></td>
-                    <td><input id="password_field" size={30} type="password" onChange={passwordChange} /></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input id="login_button" type="submit" value="LOGIN" /></td>
-                </tr>
-                </tbody>
-            </table>
-        </form>
+            <form name="login_form" onSubmit={handleSubmit} >
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><label htmlFor="username_field">User Name:</label></td>
+                            <td><input id="username_field" size={30} type="text" onChange={usernameChange} /></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="password_field">Password:</label></td>
+                            <td><input id="password_field" size={30} type="password" onChange={passwordChange} /></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input id="login_button" type="submit" value="LOGIN" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+            <button id="goes_hidden" onClick={event => goesInvisible(event)} >Visible</button>
     </>
     }
 
---
+++ atest/test/03_Waiting/wait_for_element_state.robot
@@ -0,0 +1,16 @@
+*** Settings ***
+Resource          imports.resource
+Test Setup       Go To    ${LOGIN URL}
+
+*** Test Cases ***
+Wait For Elements State to hide
+    Get Attribute    button#goes_hidden  style
+    Click  button#goes_hidden
+    Wait For Elements State    button#goes_hidden  hidden  1s
+Wait For Elements State fails on too short timeout
+    Get Attribute    button#goes_hidden  style
+    Click  button#goes_hidden
+    Run Keyword and Expect Error  *details = \"Timeout 400ms exceeded during page.waitForSelector.*  Wait For Elements State    button#goes_hidden  hidden  400ms
+
+
+
