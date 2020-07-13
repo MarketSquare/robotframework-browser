cd2ea3f9ddddd0d25c0f6198ea45023fc49ab2a5
Janne Härkönen <jth@reaktor.com>
2020-07-13 11:48:55 +0300

    Run utest/ with black as well

--- utest/test_timestr_to_millisecs.py
+++ utest/test_timestr_to_millisecs.py
@@ -2,7 +2,7 @@ from Browser.utils.time_conversion import timestr_to_millisecs
 
 
 def test_timestring_to_millisecs():
-    assert timestr_to_millisecs('1s') == 1000
+    assert timestr_to_millisecs("1s") == 1000
     assert timestr_to_millisecs(1) == 1000
     assert timestr_to_millisecs(1.0) == 1000
-    assert timestr_to_millisecs('1 hour') == 3600 * 1000
+    assert timestr_to_millisecs("1 hour") == 3600 * 1000
