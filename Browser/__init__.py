1744a1e788bbb6ae9160fc1a909bcb269387fe21
Janne Härkönen <jth@reaktor.com>
2020-07-15 08:36:53 +0300

    Do not spam warn section of log/report
    It is still useful to have the message about failed screenshot somewhere

--- Browser/__init__.py
+++ Browser/__init__.py
@@ -201,7 +201,7 @@ class Browser(DynamicCore):
             path = self.failure_screenshot_path(test_name)
             self.browser_control.take_page_screenshot(path)
         except Exception as err:
-            logger.warn(f"Was unable to take page screenshot after failure:\n{err}")
+            logger.info(f"Was unable to take page screenshot after failure:\n{err}")
 
     def failure_screenshot_path(self, test_name):
         return os.path.join(
