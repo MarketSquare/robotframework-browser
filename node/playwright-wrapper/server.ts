7f3e44f8cf65ec3cc50019814a6fd4225aa8a9e4
Janne Härkönen <jth@reaktor.com>
2020-07-22 14:10:27 +0300

    Add definition and implementation of HttpRequest
    This executes fetch on the current browser context

--- node/playwright-wrapper/server.ts
+++ node/playwright-wrapper/server.ts
@@ -264,4 +264,11 @@ export class PlaywrightServer implements IPlaywrightServer {
     ): Promise<void> {
         browserControl.setViewportSize(call, callback, this.getActivePage());
     }
+
+    async httpRequest(
+        call: ServerUnaryCall<Request.HttpRequest>,
+        callback: sendUnaryData<Response.String>,
+    ): Promise<void> {
+        evaluation.httpRequest(call, callback, this.getActivePage());
+    }
 }
