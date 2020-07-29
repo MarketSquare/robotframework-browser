88a7f585e63ec0a9e62045516a91662f3f89feb1
Tatu Aalto <aalto.tatu@gmail.com>
2020-07-30 01:28:54 +0300

    Add Cookie keyword

--- atest/test/02_Content_Keywords/cookie.robot
+++ atest/test/02_Content_Keywords/cookie.robot
@@ -9,6 +9,22 @@ Cookies From Closed Context
     ...    Tried to get all cookie's, but not context open.
     ...    Get Cookies
 
-Get Cookies
+Get Cookies Should Return Empty List When No Cookies Are Available
     ${cookies} =    Get Cookies
     Log    ${cookies}
+
+Add Cookie Without Url, Path and Domain
+    Run Keyword And Expect Error
+    ...    ValueError: Cookie should have a url or a domain/path pair.
+    ...    Add Cookie    Foo    Bar
+
+Add Cookie Should Fail If Context Is Not Open
+    ${url} =    Get Url
+    Close All Browsers
+    Run Keyword And Expect Error
+    ...    Tried to add cookie, but not context open.
+    ...    Add Cookie    Foo    Bar    url=${url}
+
+Add Cookie With Url
+    ${url} =    Get Url
+    Add Cookie    Foo    Bar    url=${url}
--- node/playwright-wrapper/cookie.ts
+++ node/playwright-wrapper/cookie.ts
@@ -2,8 +2,21 @@ import { sendUnaryData, ServerUnaryCall } from 'grpc';
 import { BrowserContext, Cookie } from 'playwright';
 import { exists } from './playwirght-invoke';
 
-import { Response } from './generated/playwright_pb';
-import { stringResponse } from './response-util';
+import { Request, Response } from './generated/playwright_pb';
+import { stringResponse, emptyWithLog } from './response-util';
+
+interface CookieData {
+    name: string;
+    value: string;
+    url?: string,
+    domain?: string,
+    path?: string,
+    expires?: number,
+    httpOnly?: boolean,
+    secure?: boolean,
+    sameSite?: 'Strict' | 'Lax' | 'None'
+}
+
 
 export async function getCookies(callback: sendUnaryData<Response.String>, context?: BrowserContext) {
     exists(context, callback, `Tried to get all cookie's, but not context open.`);
@@ -14,4 +27,15 @@ export async function getCookies(callback: sendUnaryData<Response.String>, conte
         cookieName.push(cookie.name);
     }
     callback(null, stringResponse(JSON.stringify(allCookies), cookieName.toString()));
+}
+
+export async function addCookie(
+    call: ServerUnaryCall<Request.Json>,
+    callback: sendUnaryData<Response.Empty>,
+    context?: BrowserContext,
+) {
+    exists(context, callback, `Tried to add cookie, but not context open.`);
+    const cookie: CookieData  = JSON.parse(call.request.getBody());
+    await context.addCookies([cookie]);
+    callback(null, emptyWithLog('Cookie "' + cookie.name + '" added.'));
 }
