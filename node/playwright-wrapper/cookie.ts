a8d893b790a8a8bd75afe6e1de3c87460aa56f8c
Tatu Aalto <aalto.tatu@gmail.com>
2020-07-30 00:40:23 +0300

    Fixed typo

--- node/playwright-wrapper/cookie.ts
+++ node/playwright-wrapper/cookie.ts
@@ -6,7 +6,7 @@ import { Response } from './generated/playwright_pb';
 import { stringResponse } from './response-util';
 
 export async function getCookies(callback: sendUnaryData<Response.String>, context?: BrowserContext) {
-    exists(context, callback, `Tried to get cookeis but not context open.`);
+    exists(context, callback, `Tried to get all cookie's, but not context open.`);
     const allCookies = await context.cookies();
     console.log(allCookies);
     const cookieName = [];
