af8514f753eed96cefee75df2ea5d2e575ef8612
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-08-05 09:34:13 +0300

    Remove unused force argument from Click

--- Browser/keywords/interaction.py
+++ Browser/keywords/interaction.py
@@ -100,7 +100,7 @@ class Interaction(LibraryComponent):
             logger.debug(response.log)
 
     @keyword(tags=["Setter", "PageContent"])
-    def click(self, selector: str, force: bool = False):
+    def click(self, selector: str):
         """Clicks the element found by ``selector``. """
         with self.playwright.grpc_channel() as stub:
             response = stub.Click(Request().ElementSelector(selector=selector))
