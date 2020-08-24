15f156ee82f44314f889fbfdac2dcc72ea7150f7
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-08-24 15:53:16 +0300

    Try to make data structures less verbose in playwright-state.ts

--- node/playwright-wrapper/playwright-state.ts
+++ node/playwright-wrapper/playwright-state.ts
@@ -50,14 +50,33 @@ async function _newBrowserContext(
     }
     context.setDefaultTimeout(parseFloat(process.env.TIMEOUT || '10000'));
     const id = uuidv4();
-    return { id: id, c: context, pageStack: [], options: options };
+
+    return new IndexedContext(context, id, [], new Map(), options);
 }
 
 async function _newPage(context: BrowserContext): Promise<IndexedPage> {
     const id = uuidv4();
     const newPage = await context.newPage();
-    return { id: id, p: newPage };
+    return { id: id, value: newPage };
 }
+
+const pageToCatalog = async (page: IndexedPage) => {
+    const { id, value } = page;
+    return {
+        type: 'page',
+        title: await value.title(),
+        url: value.url(),
+        id: id,
+    };
+};
+
+const contextToCatalog = async (context: IndexedContext) => {
+    return {
+        type: 'context',
+        id: context.id,
+        pages: await Promise.all([...context.children.values()].map(pageToCatalog)),
+    };
+};
 
 export class PlaywrightState {
     constructor() {
@@ -109,32 +128,11 @@ export class PlaywrightState {
     }
 
     public async getCatalog() {
-        const pageToContents = async (page: IndexedPage) => {
-            return {
-                type: 'page',
-                title: await page.p.title(),
-                url: page.p.url(),
-                id: page.id,
-            };
-        };
-
-        const contextToContents = async (context: IndexedContext) => {
-            return {
-                type: 'context',
-                id: context.id,
-                pages: await Promise.all(context.pageStack.map(pageToContents)),
-            };
-        };
-
         return Promise.all(
             this.browserStack.map(async (browser) => {
                 return {
-                    type: browser.name,
-                    id: browser.id,
-                    contexts: await Promise.all(browser.contextStack.map(contextToContents)),
-                    activePage: browser.page?.id,
-                    activeContext: browser.context?.id,
-                    activeBrowser: this.activeBrowser === browser,
+                    browser: browser.getCatalog(),
+                    active: this.activeBrowser === browser,
                 };
             }),
         );
@@ -152,11 +150,11 @@ export class PlaywrightState {
     }
 
     public getActiveContext = (): BrowserContext | undefined => {
-        return this.activeBrowser?.context?.c;
+        return this.activeBrowser?.context?.value;
     };
 
     public getActivePage = (): Page | undefined => {
-        return this.activeBrowser?.page?.p;
+        return this.activeBrowser?.page?.value;
     };
 
     public addElement(id: string, handle: ElementHandle): void {
@@ -172,37 +170,81 @@ export class PlaywrightState {
     }
 }
 
-type IndexedContext = {
-    c: BrowserContext;
+interface Stacked<T> {
+    stack: T[];
+}
+
+const stackFrom = <T>(stacked: Stacked<T>) => stacked.stack;
+const lastOfStack = <T>(t: Stacked<T>) => lastItem(stackFrom(t));
+
+interface IndexedStruct<T> {
+    type: string;
+    stack: (Uuid | undefined)[];
+    children: Map<Uuid, T>;
+}
+
+interface Indexed<T> {
     id: Uuid;
-    pageStack: IndexedPage[];
-    options?: Record<string, unknown>;
-};
+    value: T;
+}
+
+interface Closeable {
+    value: {
+        close: () => Promise<void>;
+    };
+}
+
+function push<T>(struct: IndexedStruct<Indexed<T>>, elem: Indexed<T> | undefined) {
+    if (elem) {
+        struct.children.set(elem.id, elem);
+        struct.stack.push(elem.id);
+        logger.info(`Changed active ${struct.type}`);
+    } else {
+        logger.info(`Set active ${struct.type} to undefined`);
+    }
+}
+
+async function closeRecent<T extends Closeable>(struct: IndexedStruct<T>) {
+    const id = struct.stack.pop();
+    await struct.children.get(id!)!.value.close();
+}
+
+class IndexedContext implements IndexedStruct<IndexedPage> {
+    type = 'Context';
+    constructor(
+        public value: BrowserContext,
+        public id: Uuid,
+        public stack: (Uuid | undefined)[],
+        public children: Map<Uuid, IndexedPage>,
+        public options?: Record<string, unknown>,
+    ) {}
+}
 
 type IndexedPage = {
-    p: Page;
+    value: Page;
     id: Uuid;
 };
 
 // Meta type to make it clearer where we are handling actual id's
 type Uuid = string;
 
-export class BrowserState {
+export class BrowserState implements IndexedStruct<IndexedContext> {
+    type = 'Browser';
     constructor(name: string, browser: Browser) {
         this.name = name;
         this.browser = browser;
-        this._contextStack = [];
+        this.stack = [];
+        this.children = new Map();
         this.id = uuidv4();
     }
-    private _contextStack: IndexedContext[];
     browser: Browser;
+    children: Map<Uuid, IndexedContext>;
+    stack: Uuid[];
     name?: string;
     id: Uuid;
 
     public async close(): Promise<void> {
-        this.context = undefined;
-        this.page = undefined;
-        this._contextStack = [];
+        this.stack = [];
         await this.browser.close();
     }
 
@@ -212,57 +254,39 @@ export class BrowserState {
         } else {
             const browser = this.browser;
             const context = await _newBrowserContext(browser);
-            this.context = context;
+            push(this, context);
             return context;
         }
     }
+
     get context(): IndexedContext | undefined {
-        return lastItem(this._contextStack);
-    }
-    set context(newContext: IndexedContext | undefined) {
-        if (newContext !== undefined) {
-            if (newContext.options === undefined) {
-                // copy known options
-                newContext.options = this._contextStack.find((c) => c.c === newContext.c)?.options;
-            }
-            // prevent duplicates
-            this._contextStack = this._contextStack.filter((c) => c.c !== newContext.c);
-            this._contextStack.push(newContext);
-            logger.info('Changed active context');
-        } else logger.info('Set active context to undefined');
-    }
-    get page(): IndexedPage | undefined {
-        const context = this.context;
-        if (context) return lastItem(context.pageStack);
-        else return undefined;
+        return lastOfStack(this);
     }
 
-    set page(newPage: IndexedPage | undefined) {
-        const currentContext = this.context;
-        if (newPage !== undefined && currentContext !== undefined) {
-            // prevent duplicates
-            currentContext.pageStack = currentContext.pageStack.filter((p) => p.p !== newPage.p);
-            currentContext.pageStack.push(newPage);
-            logger.info('Changed active page');
-        } else {
-            logger.info('Set active page to undefined');
-        }
+    async closeActivePage() {
+        await closeRecent(this.context);
     }
-    get contextStack(): IndexedContext[] {
-        return this._contextStack;
+
+    get page(): IndexedPage | undefined {
+        return lastOfStack(this.context);
     }
-    public popPage(): void {
-        const pageStack = this.context?.pageStack || [];
-        pageStack.pop();
+
+    setActivePage(newPage: IndexedPage | undefined) {
+        if (this.context) push(this.context, newPage);
     }
-    public popContext(): void {
-        this._contextStack.pop();
-        const context = lastItem(this._contextStack);
-        if (context) {
-            this.context = context;
-        } else {
-            this.context = undefined;
-        }
+
+    public async closeContext(): Promise<void> {
+        const id = this.stack.pop();
+        await this.children.get(id!)!.value.close();
+    }
+    public async getCatalog() {
+        return {
+            type: this.name,
+            id: this.id,
+            contexts: await Promise.all([...this.children.values()].map(contextToCatalog)),
+            activePage: this.page?.id,
+            activeContext: this.context?.id,
+        };
     }
 }
 
@@ -289,14 +313,13 @@ export async function closeContext(
 ): Promise<void> {
     const activeBrowser = openBrowsers.getActiveBrowser(callback);
     await openBrowsers.getActiveContext()?.close();
-    activeBrowser.popContext();
+    closeRecent(activeBrowser);
     callback(null, emptyWithLog('Succesfully closed Context'));
 }
 
 export async function closePage(callback: sendUnaryData<Response.Empty>, openBrowsers: PlaywrightState): Promise<void> {
     const activeBrowser = openBrowsers.getActiveBrowser(callback);
-    await openBrowsers.getActivePage()?.close();
-    activeBrowser.popPage();
+    await activeBrowser.closeActivePage();
     callback(null, emptyWithLog('Succesfully closed Page'));
 }
 
@@ -308,10 +331,10 @@ export async function newPage(
     const browserState = await openBrowsers.getOrCreateActiveBrowser();
     const context = await browserState.getOrCreateActiveContext();
 
-    const page = await _newPage(context.c);
-    browserState.page = page;
+    const page = await _newPage(context.value);
+    push(context, page);
     const url = call.request.getUrl() || 'about:blank';
-    await invokeOnPage(page.p, callback, 'goto', url, { timeout: 10000 });
+    await invokeOnPage(page.value, callback, 'goto', url, { timeout: 10000 });
     const response = stringResponse(page.id, 'New page opeened.');
     response.setLog('Succesfully initialized new page object and opened url: ' + url);
     callback(null, response);
@@ -327,7 +350,7 @@ export async function newContext(
     try {
         const options = JSON.parse(call.request.getRawoptions());
         const context = await _newBrowserContext(browserState.browser, options, hideRfBrowser);
-        browserState.context = context;
+        push(browserState, context);
 
         const response = stringResponse(context.id, 'New context opened');
         response.setLog('Succesfully created context with options: ' + JSON.stringify(options));
@@ -363,19 +386,19 @@ export async function autoActivatePages(
     browserState?: BrowserState,
 ) {
     exists(
-        browserState?.context?.c,
+        browserState?.context?.value,
         callback,
         'Tried to focus next opened page, no context was open in current Browser',
     );
 
-    browserState.context.c.on('page', (page) => {
+    browserState.context.value.on('page', (page) => {
         exists(
-            browserState?.context?.c,
+            browserState?.context?.value,
             callback,
             `Tried to focus on new page in context, context didn't exist! This should be impossible.`,
         );
         const pageIndex = uuidv4();
-        browserState.context.pageStack.push({ p: page, id: pageIndex });
+        push(browserState.context, { value: page, id: pageIndex });
         logger.info('Changed active page');
     });
     callback(null, emptyWithLog('Will focus future ``pages`` in this context'));
@@ -384,24 +407,24 @@ export async function autoActivatePages(
 async function _switchPage(id: string, browserState: BrowserState, waitForPage: boolean) {
     const context = browserState.context;
     if (!context) throw new Error('Tried to switch page, no open context');
-    const pages = context.pageStack;
+    const pages = context.children;
 
-    const page = pages.find((p) => p.id == id);
+    const page = pages.get(id);
     if (page) {
-        browserState.page = { id: id, p: page.p };
-        await page.p.bringToFront();
+        push(context, page);
+        await page.value.bringToFront();
         return;
     } else if (waitForPage) {
         try {
             logger.info('Started waiting for a page to pop up');
-            const page = await context.c.waitForEvent('page');
-            browserState.page = { id: id, p: page };
+            const page = await context.value.waitForEvent('page');
+            browserState.setActivePage({ id: id, value: page });
             await page.bringToFront();
             return;
         } catch (pwError) {
             logger.info('Wait was not fulfilled');
             logger.error(pwError);
-            const mapped = pages?.map((page) => page.p.url()).join(',');
+            const mapped = await Promise.all([...pages.values()].map(pageToCatalog));
             const message = `No page for id ${id}. Open pages: ${mapped}`;
             const error = new Error(message);
             throw error;
@@ -412,16 +435,13 @@ async function _switchPage(id: string, browserState: BrowserState, waitForPage: 
 }
 
 async function _switchContext(id: string, browserState: BrowserState) {
-    const contexts = browserState.contextStack;
-    const context = contexts.find((context) => context.id == id);
+    const contexts = browserState.children;
+    const context = contexts.get(id);
     if (contexts && context) {
-        browserState.context = context;
+        push(browserState, context);
         return;
     } else {
-        const mapped = contexts
-            ?.map((c) => c.pageStack)
-            .reduce((acc, val) => acc.concat(val), [])
-            .map((page) => page.p.url());
+        const mapped = await browserState.getCatalog();
 
         const message = `No context for id ${id}. Open contexts: ${mapped}`;
         throw new Error(message);
