a2a57e3b95a3203a8361ab9f9d3850aefebce8ec
Mikko Korpela <mikko.korpela@gmail.com>
2020-10-11 22:06:03 +0300

    lint

--- Browser/keywords/playwright_state.py
+++ Browser/keywords/playwright_state.py
@@ -78,7 +78,9 @@ class PlaywrightState(LibraryComponent):
 
         ``pauseOnFailure`` <bool> Stop execution when failure detected and leave browser open. Defaults to True.
         """
-        logger.warn("Open Browser is for quick experimentation and debugging only. Use New Page for production.")
+        logger.warn(
+            "Open Browser is for quick experimentation and debugging only. Use New Page for production."
+        )
         self.new_browser(browser, headless=headless)
         self.new_context()
         self.new_page(url)
