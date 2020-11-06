d852856b41b8cb9b539129a3ac0eb7061d2ce85c
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2020-11-06 09:54:33 +0200

    Added note about upcoming changes in 3.0 release.

--- Browser/keywords/interaction.py
+++ Browser/keywords/interaction.py
@@ -119,6 +119,10 @@ class Interaction(LibraryComponent):
         clear: bool = True,
     ):
         """Types the given ``secret`` into the text field found by ``selector``.
+        
+        NOTE: Implementation of this keyword will change in 3.0 release.
+        Keyword will only read secret value from environment variable and will
+        not be backwards compatible.
 
         The difference to `Type Text` is that this keyword does not log secret
         on the INFO level. If ``enable_playwright_debug`` is enabled in the library
@@ -155,6 +159,10 @@ class Interaction(LibraryComponent):
     @keyword(tags=["Setter", "PageContent"])
     def fill_secret(self, selector: str, secret: str):
         """Fills the given ``secret`` into the text field found by ``selector``.
+        
+        NOTE: Implementation of this keyword will change in 3.0 release.
+        Keyword will only read secret value from environment variable and will
+        not be backwards compatible.
 
         The difference to `Fill Text` is that this keyword does not log secret
         on the INFO level. If ``enable_playwright_debug`` is enabled in the library
