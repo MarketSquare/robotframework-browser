10314afda3b1a05b981c792334334104798e5e6e
Tatu Aalto <aalto.tatu@gmail.com>
2020-11-10 21:34:16 +0200

    Convert assertion engine utest to use approval tests

--- utest/test_assertion_engine.py
+++ utest/test_assertion_engine.py
@@ -1,4 +1,6 @@
 import pytest
+from approvaltests import verify_all  # type: ignore
+
 from Browser.assertion_engine import (
     verify_assertion,
     AssertionOperator,
@@ -8,7 +10,12 @@ from robot.libraries.BuiltIn import EXECUTION_CONTEXTS  # type: ignore
 
 
 def test_equals():
-    _validate_operator(AssertionOperator["=="], "actual", "actual", "unexpected")
+    results = [
+        _validate_operator_2(AssertionOperator["=="], "actual", "actual"),
+        _validate_operator_2(AssertionOperator["=="], "actual", "unexpected")
+
+    ]
+    verify_all("Test equals", results)
 
 
 def _validate_operator(operator: AssertionOperator, actual, expected, invalid):
@@ -18,12 +25,21 @@ def _validate_operator(operator: AssertionOperator, actual, expected, invalid):
 
 
 def test_not_equals():
-    _validate_operator(AssertionOperator["!="], "actual", "expected", "actual")
+    results = [
+        _validate_operator_2(AssertionOperator["!="], "actual", "expected"),
+        _validate_operator_2(AssertionOperator["!="], "actual", "actual"),
+    ]
+    verify_all("Not equal", results)
 
 
 def test_contains():
-    _validate_operator(AssertionOperator["contains"], "actual", "ctua", "nope")
-    _validate_operator(AssertionOperator["*="], "actual", "tual", "nope")
+    results = [
+        _validate_operator_2(AssertionOperator["contains"], "actual", "ctua"),
+        _validate_operator_2(AssertionOperator["contains"], "actual", "nope"),
+        _validate_operator_2(AssertionOperator["*="], "actual", "tual"),
+        _validate_operator_2(AssertionOperator["*="], "actual", "nope"),
+    ]
+    verify_all("Contains", results)
 
 
 class FakeBrowser:
@@ -33,61 +49,82 @@ class FakeBrowser:
 
     @with_assertion_polling
     def is_three(self, value):
-        verify_assertion(value, AssertionOperator["=="], 3)
+        return verify_assertion(value, AssertionOperator["=="], 3)
 
     @with_assertion_polling
     def second_run_success(self):
         current = self.counter
         self.counter += 1
-        verify_assertion(current, AssertionOperator["=="], 2)
+        return verify_assertion(current, AssertionOperator["=="], 2)
 
 
 def test_with_assertions_polling():
     fb = FakeBrowser()
-    fb.is_three(3)
-    with pytest.raises(AssertionError):
-        fb.is_three(2)
-    fb.second_run_success()
+    results = [
+        fb.is_three(3),
+        _method_validator(fb.is_three, 2),
+        fb.second_run_success(),
+    ]
+    verify_all("Assertion polling", results)
 
 
 def test_without_assertions_polling():
     fb = FakeBrowser()
     fb.retry_assertions_for = 0
-    fb.is_three(3)
-    with pytest.raises(AssertionError):
-        fb.is_three(2)
-    with pytest.raises(AssertionError):
-        fb.second_run_success()
+    results = [
+        fb.is_three(3),
+        _method_validator(fb.is_three, 2),
+        _method_validator(fb.second_run_success)
+    ]
+    verify_all("No polling", results)
 
 
 def test_greater():
-    _validate_operator(AssertionOperator["<"], 1, 2, 0)
+    results = [
+        _validate_operator_2(AssertionOperator["<"], 1, 2),
+        _validate_operator_2(AssertionOperator["<"], 1, 0),
+    ]
+    verify_all("Greater", results)
 
 
 def test_less():
-    _validate_operator(AssertionOperator[">"], 2, 1, 3)
+    results = [
+        _validate_operator_2(AssertionOperator[">"], 2, 1),
+        _validate_operator_2(AssertionOperator[">"], 2, 3),
+    ]
+    verify_all("Less", results)
 
 
 def test_greater_or_equal():
-    _validate_operator(AssertionOperator["<="], 1, 2, 0)
-    _validate_operator(AssertionOperator["<="], 1, 1, 0)
+    results = [
+        _validate_operator_2(AssertionOperator["<="], 1, 2),
+        _validate_operator_2(AssertionOperator["<="], 1, 0),
+        _validate_operator_2(AssertionOperator["<="], 1, 1),
+    ]
+    verify_all("greater or equal", results)
 
 
 def test_less_or_equal():
-    _validate_operator(AssertionOperator[">="], 2, 2, 3)
-    _validate_operator(AssertionOperator[">="], 2, 1, 3)
+    results = [
+        _validate_operator_2(AssertionOperator[">="], 2, 2),
+        _validate_operator_2(AssertionOperator[">="], 2, 3),
+        _validate_operator_2(AssertionOperator[">="], 2, 1),
+    ]
+    verify_all("less or equal", results)
 
 
 def test_match():
-    _validate_operator(AssertionOperator["matches"], "Actual", "(?i)actual", "/(\\d)+/")
-    _validate_operator(AssertionOperator["matches"], "Actual", "^Act", "/(\\d)+/")
-    _validate_operator(AssertionOperator["matches"], "Actual", "ual$", "/(\\d)+/")
-    _validate_operator(
-        AssertionOperator["matches"],
-        "Actual\nmultiline",
-        "(?m)Actual\nmultiline$",
-        "/(\\d)+/",
-    )
+    results = [
+        _validate_operator_2(AssertionOperator["matches"], "Actual", "(?i)actual"),
+        _validate_operator_2(AssertionOperator["matches"], "Actual", "/(\\d)+/"),
+        _validate_operator_2(AssertionOperator["matches"], "Actual", "^Act"),
+        _validate_operator_2(AssertionOperator["matches"], "Actual", "/(\\d)+/"),
+        _validate_operator_2(AssertionOperator["matches"], "Actual", "ual$"),
+        _validate_operator_2(AssertionOperator["matches"], "Actual\nmultiline", "(?m)Actual\nmultiline$"),
+        _validate_operator_2(AssertionOperator["matches"], "Actual\nmultiline", "/(\\d)+/"),
+
+    ]
+    verify_all("match", results)
 
 
 @pytest.fixture()
@@ -104,30 +141,57 @@ def with_suite():
 
 
 def test_validate(with_suite):
-    _validate_operator(
-        AssertionOperator("validate"), 1, "0 < value < 2", "value == 'hello'"
-    )
+    results = [
+        _validate_operator_2(AssertionOperator("validate"), 1, "0 < value < 2"),
+        _validate_operator_2(AssertionOperator("validate"), 1, "value == 'hello'"),
+    ]
+    verify_all("validate", results)
 
 
 def test_then(with_suite):
-    thenOp = AssertionOperator["then"]
-    assert verify_assertion(8, thenOp, "value + 3") == 11
-    assert verify_assertion(2, thenOp, "value + 3") == 5
-    assert verify_assertion("René", thenOp, "'Hello ' + value + '!'") == "Hello René!"
+    then_op = AssertionOperator["then"]
+    results = [
+        verify_assertion(8, then_op, "value + 3") == 11,
+        verify_assertion(2, then_op, "value + 3") == 5,
+        verify_assertion("René", then_op, "'Hello ' + value + '!'") == "Hello René!"
+    ]
+    verify_all("then", results)
 
 
 def test_start_with():
-    _validate_operator(AssertionOperator["^="], "Hello Robots", "Hello", "Robots")
-    _validate_operator(
-        AssertionOperator["should start with"], "Hello Robots", "Hello", "Robots"
-    )
-    _validate_operator(
-        AssertionOperator["^="], "Hel[4,5]?[1-9]+ Robots", "Hel[4,5]?[1-", ".*"
-    )
+    results = [
+        _validate_operator_2(AssertionOperator["^="], "Hello Robots", "Hello"),
+        _validate_operator_2(AssertionOperator["^="], "Hello Robots", "Robots"),
+        _validate_operator_2(AssertionOperator["should start with"], "Hello Robots", "Hello"),
+        _validate_operator_2(AssertionOperator["should start with"], "Hello Robots", "Robots"),
+        _validate_operator_2(AssertionOperator["^="], "Hel[4,5]?[1-9]+ Robots", "Hel[4,5]?[1-"),
+        _validate_operator_2(AssertionOperator["^="], "Hel[4,5]?[1-9]+ Robots", ".*"),
+
+    ]
+    verify_all("start with", results)
 
 
 def test_ends_with():
-    _validate_operator(AssertionOperator["$="], "Hello Robots", "Robots", "Hello")
-    _validate_operator(
-        AssertionOperator["$="], "Hel[4,5]?[1-9]+ Robots", "[1-9]+ Robots", ".*"
-    )
+    results = [
+        _validate_operator_2(AssertionOperator["$="], "Hello Robots", "Robots"),
+        _validate_operator_2(AssertionOperator["$="], "Hello Robots", "Hello"),
+        _validate_operator_2(AssertionOperator["$="], "Hel[4,5]?[1-9]+ Robots", "[1-9]+ Robots"),
+        _validate_operator_2(AssertionOperator["$="], "Hel[4,5]?[1-9]+ Robots", ".*"),
+    ]
+    verify_all("ends with", results)
+
+
+def _validate_operator_2(operator: AssertionOperator, actual, expected):
+    try:
+        return verify_assertion(actual, operator, expected)
+    except Exception as error:
+        return error
+
+
+def _method_validator(method, arg=None):
+    try:
+        if arg is None:
+            return method()
+        return method(arg)
+    except Exception as error:
+        return error
