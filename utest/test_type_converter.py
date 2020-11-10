e14c008afc74877f5dc5dd3ccb7ebab235ce5610
Tatu Aalto <aalto.tatu@gmail.com>
2020-11-10 20:59:17 +0200

    Type as string

---
+++ utest/test_type_converter.py
@@ -0,0 +1,13 @@
+from approvaltests import verify_all  # type: ignore
+
+from Browser.utils.misc import type_converter
+
+
+def test_type_converter():
+    results = [
+        type_converter(None),
+        type_converter(""),
+        type_converter(1),
+        type_converter(True),
+    ]
+    verify_all("Type converter", results)
