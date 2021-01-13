78a52b8f60e17a64ff8f52f3fcff2f7d3b04f9bd
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2021-01-13 09:45:29 +0200

    Whitespace cleanup

--- Browser/keywords/interaction.py
+++ Browser/keywords/interaction.py
@@ -583,8 +583,8 @@ class Interaction(LibraryComponent):
 
     @keyword(tags=("PageContent",))
     def handle_future_dialogs(self, action: DialogAction, prompt_input: str = ""):
-        """Handle next dialog on page with ``action``. 
-        
+        """Handle next dialog on page with ``action``.
+
         Dialog can be any of alert, beforeunload, confirm or prompt. Handling dialogue
         must be called before the action, like example click, that triggers the
         dialogue.
@@ -593,9 +593,9 @@ class Interaction(LibraryComponent):
 
             ``prompt_input`` The value to enter into prompt. Only valid if
             ``action`` equals accept. Defaults to empty string.
-        
+
         Example:
-        
+
         | Handle Future Dialogs    action=accept
         | Click                    \#alerts
         """
