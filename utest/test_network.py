caf3daf18f742e3ba3a2f70503242cbf8180c6e6
Mikko Korpela <mikko.korpela@gmail.com>
2021-03-06 21:45:59 +0200

    test for json response parsing (#773)

---
+++ utest/test_network.py
@@ -0,0 +1,25 @@
+from Browser.keywords.network import _format_response
+
+
+def test_response_parsing_lowercase():
+    response = _format_response({
+        "headers": '{"content-type": "application/json"}',
+        "body": '{"key":"value1"}'
+    })
+    assert response['body']['key'] == 'value1'
+
+
+def test_response_parsing_caps():
+    response = _format_response({
+        "headers": '{"Content-Type": "application/json"}',
+        "body": '{"key":"value2"}'
+    })
+    assert response['body']['key'] == 'value2'
+
+
+def test_response_parsing_text():
+    response = _format_response({
+        "headers": '{"Content-Type": "text/html; charset=UTF-8"}',
+        "body": "{key:'value3'}"
+    })
+    assert response['body'] == "{key:'value3'}"
