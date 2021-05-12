deae56af1516dcd3c5ca109d51152b1e1500091c
Tatu Aalto <aalto.tatu@gmail.com>
2021-05-12 22:26:24 +0300

    Make node mock more generic

--- utest/test_get_text.py
+++ utest/test_get_text.py
@@ -3,18 +3,6 @@ from unittest.mock import MagicMock
 from Browser.keywords import Getters
 
 
-def test_get_text():
-    ctx = MagicMock()
-    pw = MagicMock()
-    grpc = MagicMock()
-    get_text = MagicMock()
-    response = MagicMock()
-    response.log = ""
-    response.body = ""
-    get_text.GetText = MagicMock(return_value=response)
-    enter = MagicMock(return_value=get_text)
-    grpc.__enter__ = enter
-    pw.grpc_channel.return_value = grpc
-    ctx.playwright = pw
+def test_get_text(ctx: MagicMock):
     getter = Getters(ctx)
     getter.get_text("//div")
