432dd12568ff08e3ed4b24b0c054094994303344
Tatu Aalto <aalto.tatu@gmail.com>
2021-05-13 00:11:15 +0300

    Make wait for function to poll

--- utest/test_waiters.py
+++ utest/test_waiters.py
@@ -1,7 +1,5 @@
 from unittest.mock import MagicMock, PropertyMock
 
-import pytest
-
 from Browser import ElementState
 from Browser.keywords import Waiter
 
@@ -18,7 +16,7 @@ def test_wait_for_state_error():
     grpc = MagicMock()
     get_text = MagicMock()
     response = MagicMock()
-    type(response).log = PropertyMock(side_effect=[AssertionError, "One"])
+    type(response).log = PropertyMock(side_effect=[AssertionError, "one"])
     get_text.WaitForElementsState = MagicMock(return_value=response)
     enter = MagicMock(return_value=get_text)
     grpc.__enter__ = enter
@@ -28,20 +26,18 @@ def test_wait_for_state_error():
     wait.wait_for_elements_state("id=myText")
 
 
-def test_wait_for_state_error():
+def test_wait_for_function():
     ctx = MagicMock()
     ctx.timeout = 1000
     pw = MagicMock()
     grpc = MagicMock()
     get_text = MagicMock()
     response = MagicMock()
-    type(response).log = PropertyMock(side_effect=[AssertionError, "One"])
-    get_text.WaitForElementsState = MagicMock(return_value=response)
+    type(response).log = PropertyMock(side_effect=[AssertionError, "10"])
+    get_text.WaitForFunction = MagicMock(return_value=response)
     enter = MagicMock(return_value=get_text)
     grpc.__enter__ = enter
     pw.grpc_channel.return_value = grpc
     ctx.playwright = pw
     wait = Waiter(ctx)
     wait.wait_for_elements_state("id=myText", ElementState.checked)
-
-
