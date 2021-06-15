2094c42fbe6498183e03987e5081504044cb333c
Tatu Aalto <aalto.tatu@gmail.com>
2021-06-15 23:09:08 +0300

    Fix entry point in setup

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup_kwargs = {
     "packages": packages,
     "package_data": package_data,
     "install_requires": install_requires,
-    "entry_points": {"console_scripts": ["rfbrowser=Browser.entry:run"]},
+    "entry_points": {"console_scripts": ["rfbrowser=Browser.entry"]},
     "python_requires": ">=3.7,<4.0",
     "classifiers": [
         "Development Status :: 5 - Production/Stable",
