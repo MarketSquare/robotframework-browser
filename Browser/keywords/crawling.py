9ad1441cd2067712fec694a5a8052a35a1b161b3
Mikko Korpela <mikko.korpela@gmail.com>
2021-07-19 01:55:17 +0300

    crawling away from handling

--- Browser/keywords/crawling.py
+++ Browser/keywords/crawling.py
@@ -11,7 +11,7 @@ from ..utils import logger
 class Crawling(LibraryComponent):
     @keyword(tags=["Crawling"])
     def crawl_site(
-        self, url: Optional[str] = None, page_crawl_keyword="basic_page_crawl"
+        self, url: Optional[str] = None, page_crawl_keyword="take_screenshot"
     ):
         """
         Take screenshots from all urls inside a specific site.
@@ -19,15 +19,6 @@ class Crawling(LibraryComponent):
         if url:
             self.library.new_page(url)
         self._crawl(self.library.get_url() or "", page_crawl_keyword)
-
-    @keyword(tags=["Crawling"])
-    def basic_page_crawl(self):
-        self.library.take_screenshot()
-        link_elements = self.library.get_elements("//a[@href]")
-        links = set()
-        for link_element in link_elements:
-            links.add(self.library.execute_javascript("(e) => e.href", link_element))
-        return list(links)
 
     def _crawl(self, baseurl: str, page_crawl_keyword: str):
         hrefs_to_crawl = [baseurl]
@@ -48,7 +39,12 @@ class Crawling(LibraryComponent):
                 f"{len(crawled) + 1} / {len(crawled) + 1 + len(hrefs_to_crawl)} : Crawling url {href}"
             )
             self.library.go_to(href)
-            child_hrefs = BuiltIn().run_keyword(page_crawl_keyword)
+            BuiltIn().run_keyword(page_crawl_keyword)
+            link_elements = self.library.get_elements("//a[@href]")
+            links = set()
+            for link_element in link_elements:
+                links.add(self.library.execute_javascript("(e) => e.href", link_element))
+            child_hrefs = list(links)
             crawled.add(href)
             hrefs_to_crawl = self._build_urls_to_crawl(
                 child_hrefs, hrefs_to_crawl, crawled, baseurl
