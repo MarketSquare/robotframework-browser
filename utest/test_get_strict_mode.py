937b42e126609a44465fceebd639ec400217ae52
Tatu Aalto <aalto.tatu@gmail.com>
2021-08-25 21:52:42 +0300

    Get library srtict mode

---
+++ utest/test_get_strict_mode.py
@@ -0,0 +1,17 @@
+import pytest
+
+import Browser
+
+
+@pytest.fixture(scope="module")
+def browser():
+    return Browser.Browser()
+
+
+def test_get_strict_mode(browser: Browser):
+    browser.set_strict_mode(True)
+    assert browser.get_strict_mode(None) is True
+    assert browser.get_strict_mode(True) is True
+    assert browser.get_strict_mode(False) is False
+    browser.set_strict_mode(False)
+    assert browser.get_strict_mode(True) is True
