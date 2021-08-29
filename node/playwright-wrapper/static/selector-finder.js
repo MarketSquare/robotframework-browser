78b59d9905027e88c7c52e8186034152b1797387
Mikko Korpela <mikko.korpela@gmail.com>
2021-08-29 19:39:45 +0300

    movable

--- node/playwright-wrapper/static/selector-finder.js
+++ node/playwright-wrapper/static/selector-finder.js
@@ -201,6 +201,7 @@ function addElement (label) {
   newDiv.style.top = "16px";
   newDiv.style.left = "16px";
   newDiv.style.padding = "8px";
+  newDiv.style.cursor = "move";
   newDiv.id = BROWSER_LIBRARY_ID;
   const header = document.createElement("h5");
   header.textContent = "Selector recorder" + (label && label.length ? " for " + label : "");
@@ -212,7 +213,43 @@ function addElement (label) {
   const desc = document.createElement("span");
   desc.textContent = "Click focus to page and press (s) to record a selector.";
   newDiv.appendChild(desc);
-  document.body.appendChild(newDiv);
+  const elem = document.body.appendChild(newDiv);
+  dragElement(elem);
+}
+
+function dragElement(elmnt) {
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 window.selectorRecorderFindSelector = function(label) {
