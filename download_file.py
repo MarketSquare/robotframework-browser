a77e75c6d450a291c5e178a60cb94445c8d2f088
Tatu Aalto <aalto.tatu@gmail.com>
2021-09-03 21:47:23 +0300

    Lint fixes for download_file.py

--- download_file.py
+++ download_file.py
@@ -44,7 +44,9 @@ def get_artifacts(url: str) -> dict:
     return data["artifacts"]
 
 
-def get_artifact(artifact: dict, saved_artifacts: list, download_folder: Path, headers: dict) -> Union[Artifact, None]:
+def get_artifact(
+    artifact: dict, saved_artifacts: list, download_folder: Path, headers: dict
+) -> Union[Artifact, None]:
     url: str = artifact["archive_download_url"]
     name: str = artifact["name"]
     artifact_id = artifact["id"]
@@ -76,13 +78,23 @@ def get_xunit(zip_artifact: Artifact, xunit_folder: Path) -> list:
         for name in zip_file.namelist():
             xunit = XUnitFiles(None, None)
             if name.endswith(".xml") and "xunit" in name:
-                xunit_files.append(_xunit(zip_file, zip_artifact, xunit, name, xunit_folder))
+                xunit_files.append(
+                    _xunit(zip_file, zip_artifact, xunit, name, xunit_folder)
+                )
             if name.endswith("zip"):
-                xunit_files.extend(_extract_zip_from_zip(zip_file, name, zip_artifact, xunit_folder))
+                xunit_files.extend(
+                    _extract_zip_from_zip(zip_file, name, zip_artifact, xunit_folder)
+                )
     return list(filter(lambda item: item != [], xunit_files))
 
 
-def _xunit(zip_file: ZipFile, zip_artifact: Artifact, xunit: XUnitFiles, name: str, xunit_folder: Path) -> XUnitFiles:
+def _xunit(
+    zip_file: ZipFile,
+    zip_artifact: Artifact,
+    xunit: XUnitFiles,
+    name: str,
+    xunit_folder: Path,
+) -> XUnitFiles:
     unique_name = name.replace(".xml", "")
     unique_name = str(xunit_folder / f"{unique_name}-{zip_artifact.id}.xml")
     with tempfile.TemporaryDirectory() as tmp_folder:
@@ -92,7 +104,9 @@ def _xunit(zip_file: ZipFile, zip_artifact: Artifact, xunit: XUnitFiles, name: s
         if name.startswith("robot"):
             tree = ET.parse(unique_name)
             root = tree.getroot()
-            root.attrib["timestamp"] = zip_artifact.created_date.strftime("%Y-%m-%dT%H:%M:%S.000000")
+            root.attrib["timestamp"] = zip_artifact.created_date.strftime(
+                "%Y-%m-%dT%H:%M:%S.000000"
+            )
             new_root = ET.Element("testsuites")
             new_root.insert(0, root)
             ET.ElementTree(new_root).write(unique_name)
@@ -103,19 +117,27 @@ def _xunit(zip_file: ZipFile, zip_artifact: Artifact, xunit: XUnitFiles, name: s
     return xunit
 
 
-def _extract_zip_from_zip(zip_file: ZipFile, name: str, artifact: Artifact, xunit_folder: Path):
+def _extract_zip_from_zip(
+    zip_file: ZipFile, name: str, artifact: Artifact, xunit_folder: Path
+):
     with tempfile.TemporaryDirectory() as tmp_folder:
         zip_file.extract(name, tmp_folder)
         extracted_file = Path(tmp_folder) / name
         name_id = f"{artifact.id}-{name.replace('.zip', '')}"
-        return get_xunit(Artifact(extracted_file, name_id, artifact.created_date), xunit_folder)
+        return get_xunit(
+            Artifact(extracted_file, name_id, artifact.created_date), xunit_folder
+        )
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Download artifacts from GitHub actions")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download artifacts from GitHub actions"
+    )
     parser.add_argument("--project", help="project name in GitHub", required=True)
     parser.add_argument("--repo", help="Repository name in GitHub", required=True)
-    parser.add_argument("--token", help="GitHub token for reading artifacts", required=True)
+    parser.add_argument(
+        "--token", help="GitHub token for reading artifacts", required=True
+    )
     args = parser.parse_args()
     headers = {"Authorization": f"token {args.token}"}
     url = f"https://api.github.com/repos/{args.project}/{args.repo}"
@@ -124,7 +146,9 @@ if __name__ == '__main__':
     XUNIT_FOLDER.mkdir(exist_ok=True, parents=True)
     artifact_files = []
     for artifact in get_artifacts(url):
-        artifact_files.append(get_artifact(artifact, SAVED_ARTIFACTS, DOWNLOAD_FOLDER, headers))
+        artifact_files.append(
+            get_artifact(artifact, SAVED_ARTIFACTS, DOWNLOAD_FOLDER, headers)
+        )
     artifact_files = list(filter(lambda item: item is not None, artifact_files))
     xunit_files = []
     for artifact in artifact_files:
