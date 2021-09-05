3737fd85b151e48fccc0f649efb1a39d0d91575b
Tatu Aalto <aalto.tatu@gmail.com>
2021-09-05 16:58:33 +0300

    If stric is not bool, raise error

--- utest/test_get_strict_mode.py
+++ utest/test_get_strict_mode.py
@@ -15,3 +15,12 @@ def test_get_strict_mode(browser: Browser.Browser):
     assert browser.get_strict_mode(False) is False
     browser.set_strict_mode(False)
     assert browser.get_strict_mode(True) is True
+
+
+def test_get_strict_mode_with_error(browser: Browser.Browser):
+    browser.set_strict_mode(True)
+    with pytest.raises(ValueError):
+        browser.get_strict_mode("None")  # type: ignore
+    with pytest.raises(ValueError):
+        browser.get_strict_mode("True")  # type: ignore
+
