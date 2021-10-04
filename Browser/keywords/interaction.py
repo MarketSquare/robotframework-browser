6d6c5fc912e4a96c71859eca3eeccf5d7efa1c1b
UliSei <89480399+UliSei@users.noreply.github.com>
2021-10-04 17:21:59 +0200

    issue 1328 Docu keywords
    No coding changed, just docu for keywords
    go_to
    type_secret
    select_options_by
    drag_and_drop_by_coordinates

--- Browser/keywords/interaction.py
+++ Browser/keywords/interaction.py
@@ -137,7 +137,7 @@ class Interaction(LibraryComponent):
         found by ``selector``.
 
         This keyword does not log secret in Robot Framework logs, if keyword resolves
-        the variable valu internally. If ``enable_playwright_debug`` is enabled in
+        the variable value internally. If ``enable_playwright_debug`` is enabled in
         the library import, secret will be always visible as plain text in the playwright
         debug logs, regardless of the Robot Framework log level.
 
@@ -162,7 +162,7 @@ class Interaction(LibraryComponent):
 
         Example
         | `Type Secret`    input#username_field    $username      # Keyword resolves ${username} variable value from Robot Framework variables
-        | `Type Secret`    input#username_field    %username      # Keyword resolves $USERANME/%USERANME% variable value from environment variables
+        | `Type Secret`    input#username_field    %username      # Keyword resolves $USERNAME/%USERNAME% variable value from environment variables
         | `Type Secret`    input#username_field    ${username}    # Robot Framework resolves the variable value, but secrect can leak to Robot framework output files.
         """
         originals = self._get_original_values(locals())
@@ -585,10 +585,10 @@ class Interaction(LibraryComponent):
         If no values to select are passed will deselect options in element.
 
         Example:
-        | `Select Options By`    select[name=preferred_channel]    label    Direct mail
-        | `Select Options By`    select[name=interests]            value    males    females    others
-        | `Select Options By`    select[name=possible_channels]    index    0    2
-        | `Select Options By`    select[name=interests]            text    Males    Females
+        | `Select Options By`    label    select[name=preferred_channel]    Direct mail
+        | `Select Options By`    value    select[name=interests]    males    females    others
+        | `Select Options By`    index    select[name=possible_channels]    0    2
+        | `Select Options By`    text     select[name=interests]    Males    Females
         """
         matchers = ""
         if not values or len(values) == 1 and not values[0]:
@@ -839,9 +839,9 @@ class Interaction(LibraryComponent):
         Start- and end-point are defined by ``x`` and ``y`` coordinates relative to
         the top left corner of the pages viewport.
 
-        ``from_x`` & ``from_y`` identify the the start-point.
+        ``from_x`` & ``from_y`` identify the start-point.
 
-        ``to_x`` & ``to_y`` identify the the end-point.
+        ``to_x`` & ``to_y`` identify the end-point.
 
         ``steps`` defines how many intermediate mouse move events are sent.
 
