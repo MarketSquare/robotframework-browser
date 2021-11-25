67d095c6f6cd30dae0a6361783c50c17a3d10886
Tatu Aalto <aalto.tatu@gmail.com>
2021-11-25 21:57:46 +0200

    Log when keywords calls method from node side

---
+++ node/playwright-wrapper/keyword-decorators.ts
@@ -0,0 +1,34 @@
+import { pino } from 'pino';
+const logger = pino({ timestamp: pino.stdTimeFunctions.isoTime });
+
+// Idea and async logger method from https://github.com/norbornen/execution-time-decorator/
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function class_async_logger(target: Function) {
+    for (const propertyName of Object.keys(target.prototype)) {
+        const descriptor = Object.getOwnPropertyDescriptor(target.prototype, propertyName);
+        const isMethod = descriptor?.value instanceof Function;
+        if (!isMethod || !descriptor) continue;
+
+        const timered_method = async_logger(propertyName, descriptor);
+        Object.defineProperty(target.prototype, propertyName, timered_method);
+    }
+}
+
+export function async_logger(
+    propertyKey: string,
+    propertyDescriptor: PropertyDescriptor,
+): PropertyDescriptor {
+    const originalMethod = propertyDescriptor.value;
+    propertyDescriptor.value = async function (...args: any[]) {
+        try {
+            logger.info(`Start of node method ${propertyKey}`);
+            const result = await originalMethod.apply(this, args);
+            logger.info(`End of node method ${propertyKey}`);
+            return result;
+        } catch (err) {
+            logger.info(`Error of node method  ${propertyKey}`);
+            throw err;
+        }
+    };
+    return propertyDescriptor;
+}
