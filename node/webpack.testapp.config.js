dfb27ea66fcaed91bb97845f833e42fcdae2bde5
Mikko Korpela <mikko.korpela@gmail.com>
2021-11-27 15:36:17 +0200

    Esbuild (#1556)
    * pino is a dependency in prod
    
    * esbuild draft
    
    * build wrapper with esbuild
    
    * ignore .built flag
    
    * create-test-app in gh
    
    * target es2019 for node 12

--- node/webpack.testapp.config.js
+++ node/webpack.testapp.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
 
 const sharedAll = {
     mode: 'development',
@@ -38,47 +37,19 @@ const testappFrontend = {
     ...sharedAll
 };
 
-const sharedNode = {
-    target: 'node',
-    node: {
-        global: false,
-        __filename: false,
-        __dirname: false,
-    },
-}
-
 const testappBackend = {
     entry: './node/dynamic-test-app/src/server.ts',
     output: {
         filename: 'server.js',
         path: path.resolve(__dirname, './dynamic-test-app/dist')
     },
-    ...sharedNode,
+    target: 'node',
+    node: {
+        global: false,
+        __filename: false,
+        __dirname: false,
+    },
     ...sharedAll,
 }
 
-const playwrightWrapper = {
-    entry: './node/playwright-wrapper/index.ts',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, '../Browser/wrapper')
-    },
-    externals: {
-        grpc: 'commonjs grpc',
-        playwright: 'commonjs playwright'
-    },
-    plugins: [
-        new CopyPlugin({
-          patterns: [
-            {
-                from: "./node/playwright-wrapper/static",
-                to: path.resolve(__dirname, '../Browser/wrapper/static')
-            },
-          ],
-        }),
-    ],
-    ...sharedNode,
-    ...sharedAll,
-}
-
-module.exports = [testappFrontend, testappBackend, playwrightWrapper]
+module.exports = [testappFrontend, testappBackend]
