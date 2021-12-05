189f9d06d3d52d06f89dc052a28ac662b8445016
Tatu Aalto <aalto.tatu@gmail.com>
2021-12-06 01:06:11 +0200

    Lint fixes

--- node/playwright-wrapper/interaction.ts
+++ node/playwright-wrapper/interaction.ts
@@ -16,8 +16,8 @@ import { Dialog, Page } from 'playwright';
 
 import { PlaywrightState } from './playwright-state';
 import { Request, Response } from './generated/playwright_pb';
-import { emptyWithLog, stringResponse, jsonResponse } from './response-util';
-import { findLocator, invokeOnKeyboard, invokeOnMouse, exists } from './playwright-invoke';
+import { emptyWithLog, jsonResponse, stringResponse } from './response-util';
+import { exists, findLocator, invokeOnKeyboard, invokeOnMouse } from './playwright-invoke';
 
 import pino from 'pino';
 const logger = pino({ timestamp: pino.stdTimeFunctions.isoTime });
@@ -35,15 +35,18 @@ export async function selectOption(
         logger.info("Couldn't select any options");
         throw new Error(`No options matched ${matcher}`);
     }
-    const attributeName = Object.keys(matcher[0])[0]
-    let selectedOptions = []
+    const attributeName = Object.keys(matcher[0])[0];
+    const selectedOptions = [];
     for (const selectedOption of result) {
         const locatorOptions = locator.locator(`option[value="${selectedOption}"]`);
         const element = await locatorOptions.elementHandle();
         exists(element, `The ${selectedOption} option element did not exist.`);
         selectedOptions.push(String(await element.getProperty(attributeName)));
     }
-    return jsonResponse(JSON.stringify(selectedOptions), `Selected options [${selectedOptions}] in element ${selector}`);
+    return jsonResponse(
+        JSON.stringify(selectedOptions),
+        `Selected options [${selectedOptions}] in element ${selector}`,
+    );
 }
 
 export async function deSelectOption(
