01db520250f3bcab8328b3f94b1e1020000248a1
Mikko Korpela <mikko.korpela@gmail.com>
2022-03-06 23:41:27 +0200

    Get parameter names (#1834)
    js extension parameter names

--- node/playwright-wrapper/response-util.ts
+++ node/playwright-wrapper/response-util.ts
@@ -89,10 +89,16 @@ export function errorResponse(e: unknown) {
     return { code: errorCode, message: errorMessage };
 }
 
-export function keywordsResponse(keywords: string[], keywordDocs: string[], logMessage: string) {
+export function keywordsResponse(
+    keywords: string[],
+    keywordArguments: string[],
+    keywordDocs: string[],
+    logMessage: string,
+) {
     const response = new Response.Keywords();
     response.setKeywordsList(keywords);
     response.setKeyworddocumentationsList(keywordDocs);
+    response.setKeywordargumentsList(keywordArguments);
     response.setLog(logMessage);
     return response;
 }
