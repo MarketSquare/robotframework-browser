c5eb2d39df6f30fb35e4789be95c86673726b0ee
Tatu Aalto <aalto.tatu@gmail.com>
2022-04-07 22:25:50 +0300

    Do outputdir/Browser folder cleanup only at suite startup
    Fixes #1889

---
+++ utest/test_browser_folder_cleanup.py
@@ -0,0 +1,35 @@
+import tempfile
+from pathlib import Path
+from unittest.mock import patch, PropertyMock
+
+import pytest
+
+
+@pytest.fixture()
+def browser():
+    import Browser
+    return Browser.Browser()
+
+
+def test_cleanup_browser_folder_no_folder(browser):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        browser_folder = Path(tmp_dir) / "browser"
+        assert not browser_folder.is_dir()
+        browser_folder.mkdir()
+        with patch("Browser.Browser.outputdir", new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = tmp_dir
+            browser._start_suite(None, None)
+            assert not browser_folder.is_dir()
+
+
+def test_cleanup_browser_folder_folder(browser):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        browser_folder = Path(tmp_dir) / "browser"
+        assert not browser_folder.is_dir()
+        with patch("Browser.Browser.outputdir", new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = tmp_dir
+            browser._start_suite(None, None)
+            assert not browser_folder.is_dir()
+            browser_folder.mkdir()
+            browser._start_suite(None, None)
+            assert browser_folder.is_dir()
