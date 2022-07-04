6dbabfa311b466419ef8508614624306587712f6
Tatu Aalto <aalto.tatu@gmail.com>
2022-07-04 20:29:56 +0300

    Do not fail install, if logging fails
    Supress error and log it

--- Browser/entry.py
+++ Browser/entry.py
@@ -150,7 +150,10 @@ def _rfbrowser_init(skip_browser_install: bool):
     while process.poll() is None:
         if process.stdout:
             output = process.stdout.readline().decode("UTF-8")
-            logging.info(output)
+            try:
+                logging.info(output)
+            except Exception as error:
+                logging.info(f"While writing log file, got error: {error}")
 
     if process.returncode != 0:
         raise RuntimeError(
