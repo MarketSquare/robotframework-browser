c05a974c32c81b625fb5760e8e23a6591def36c2
Kerkko Pelttari <kerkko@robocorp.com>
2023-01-16 09:50:51 +0200

    let esbuild 0.17 work, start using the `context` function

--- node/build.wrapper.js
+++ node/build.wrapper.js
@@ -2,12 +2,13 @@
 const esbuild = require('esbuild');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
 
-esbuild.build({
-    logLevel: "info",
-    entryPoints: ["./node/playwright-wrapper/index.ts"],
-    bundle: true,
-    platform: "node",
-    outfile: "./Browser/wrapper/index.js",
-    plugins: [nodeExternalsPlugin()],
-  })
-  .catch(() => process.exit(1));
+const context = esbuild.context({
+  logLevel: "info",
+  entryPoints: ["./node/playwright-wrapper/index.ts"],
+  bundle: true,
+  platform: "node",
+  outfile: "./Browser/wrapper/index.js",
+  plugins: [nodeExternalsPlugin()],
+}).catch(() => process.exit(1));
+
+context.build().catch(() => process.exit(1));
