d02cbab101c1c65329154ebb52679078df7130aa
René Rohner <snooz@posteo.de>
2023-03-14 11:31:27 +0100

    fixed utest
    Signed-off-by: René Rohner <snooz@posteo.de>

--- utest/test_python_usage.py
+++ utest/test_python_usage.py
@@ -102,7 +102,7 @@ def test_promise_to_wait_for_response_with_name_arguments(browser):
 def test_promise_to_wait_for_alert_with_name_arguments(browser):
     browser.new_page()
     promise = browser.promise_to(
-        "Wait For Alert", "action=ignore", "prompt_input=Kala", "text=Wrong Text"
+        "Wait For Alert", "action=accept", "prompt_input=Kala", "text=Wrong Text"
     )
     browser.go_to(url="https://www.google.com")
     assert (promise.running() or promise.done()) is True
