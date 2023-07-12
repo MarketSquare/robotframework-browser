4c6f5f04043f3fd9aec72b5625c145d9073c045e
René <snooz@posteo.de>
2023-07-12 14:07:59 +0200

    Updateversion to: 17.1.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = open(os.path.join("Browser", "requirements.txt")).readlines()
 
 setup_kwargs = {
     "name": "robotframework-browser",
-    "version": "17.0.0",
+    "version": "17.1.0",
     "description": "Robot Framework Browser library powered by Playwright. Aiming for speed, reliability and visibility.",
     "long_description": long_description,
     "long_description_content_type": "text/markdown",
