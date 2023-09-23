44b752972cf78d583d9aaa7c7b4ce7c82d2dc631
Tatu Aalto <aalto.tatu@gmail.com>
2023-09-23 23:45:16 +0300

    Better entry point
    Better argument handling and support for install individual browsers
    
    Do with help of Python click

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup_kwargs = {
     "packages": packages,
     "package_data": package_data,
     "install_requires": install_requires,
-    "entry_points": {"console_scripts": ["rfbrowser=Browser.entry:main"]},
+    "entry_points": {"console_scripts": ["rfbrowser=Browser.entry:cli"]},
     "python_requires": ">=3.8,<4.0",
     "classifiers": [
         "Development Status :: 5 - Production/Stable",
