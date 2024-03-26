2789749ddea70164901471d1d15752308eff28fd
Tatu Aalto <aalto.tatu@gmail.com>
2024-03-26 23:06:53 +0200

    Testing translation

---
+++ utest/robotframework_browser_translation_not_working/__init__.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def get_language_not_working() -> dict:
+    curr_dir = Path(__file__).parent.absolute()
+    return {
+        "language": "fi",
+        "path": curr_dir / "translate.json"
+    }
