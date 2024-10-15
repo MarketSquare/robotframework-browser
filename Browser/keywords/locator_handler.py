2e390a067220c4275b0251eccd25b7afdbbb9472
Tatu Aalto <aalto.tatu@gmail.com>
2024-10-15 21:18:36 +0300

    Support more options for click method in locator handler

--- Browser/keywords/locator_handler.py
+++ Browser/keywords/locator_handler.py
@@ -23,14 +23,34 @@ class LocatorHandler(LibraryComponent):
 
     @keyword(tags=("Setter", "PageContent"))
     def add_locator_handler(
-        self, selector: str, noWaitAfter: bool = True, times: Optional[int] = None
+        self,
+        selector: str,
+        click_locator: str,
+        *,
+        noWaitAfter: bool = True,
+        times: Optional[int] = None,
+        click_clickCount: int = 1,
+        click_delay: int = 0,
+        click_force: bool = False,
+        position_x: Optional[float] = None,
+        position_y: Optional[float] = None,
     ):
         """Add locator handle which will click the element indicated by locator.
 
         | =Arguments= | =Description= |
-        | ``selector`` | Is the locator to the element to be clicked. |
-        | ``noWaitAfter`` | By default, after calling the handler Playwright will wait until the overlay becomes hidden, and only then Playwright will continue with the action/assertion that triggered the handler. This option allows to opt-out of this behavior, so that overlay can stay visible after the handler has run. |
-        | ``times`` | Is the number of times to click the element. None is unlimited. |
+        | ``selector`` | Is the selector to the element which indicated that locator handler should be called. |
+        | ``noWaitAfter`` | By default, after calling the handler Playwright will wait until the overlay becomes hidden, and only then library will continue with the action/assertion that triggered the handler. This option allows to opt-out of this behavior, so that overlay can stay visible after the handler has run. |
+        | ``times`` | Is the number of times to how often locator handler is is called. None is unlimited. |
+        | ``click_locator`` | Is the selector to the element to be clicked. |
+        | ``click_clickCount`` | Is the number of times to click the element. |
+        | ``click_delay`` | Time to wait between mousedown and mouseup in milliseconds. Defaults to 0. |
+        | ``click_force`` | Whether to bypass checks and dispatch the event directly. Defaults to false. |
+        | ``position_x`` | A point to use relative to the top-left corner of element padding box. If not specified, uses some visible point of the element. |
+        | ``position_y`` | A point to use relative to the top-left corner of element padding box. If not specified, uses some visible point of the element. |
+
+        The arguments `click_clickCount`, `click_delay`, `click_force` `position_x` and `position_y`
+        are for Playwright's `click` method. The `selector`, `noWaitAfter` and `times` are for
+        the locator handler.
 
         Example
         | `New Page`    ${URL}
@@ -40,12 +60,26 @@ class LocatorHandler(LibraryComponent):
         | `Click`    id:login
         | `Remove Locator Handler`    id:button    # Removes the locator handler from page
         """
+        if position_x and position_x:
+            click_position = Request.Position(x=str(position_x), y=str(position_y))
+        elif position_x is None or position_y is None:
+            raise ValueError(
+                f"Both position_x and position_y must be given, now position_x is {position_x} "
+                f"{type(position_x)} and position_y is {position_y} {type(position_y)}"
+            )
+        else:
+            click_position = Request.Position(x="None", y="None")
         with self.playwright.grpc_channel() as stub:
             response = stub.AddLocatorHandler(
                 Request.LocatorHandlerAdd(
                     selector=selector,
                     noWaitAfter=noWaitAfter,
                     times=str(times) if times is not None else "",
+                    clickLocator=click_locator,
+                    clickClickCount=click_clickCount,
+                    clickDelay=click_delay,
+                    clickForce=click_force,
+                    clickPosition=click_position,
                 )
             )
             logger.info(response.log)
