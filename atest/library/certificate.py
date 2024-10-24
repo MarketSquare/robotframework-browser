9409eade61fd845627c47ed1fce00da62be551c1
Ondřej Kraus <Ondrej.Kraus@aricoma.com>
2024-10-25 01:11:14 +0200

    initial tests for generating certificates

---
+++ atest/library/certificate.py
@@ -0,0 +1,220 @@
+import datetime
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+
+def generate_ca_certificate(cert_path: str, key_path: str, key_passphrase: str):
+
+    # generate CA key
+    ca_key = ec.generate_private_key(ec.SECP256R1())
+
+    # write CA key to the file
+    with open(key_path, "wb") as f:
+        f.write(ca_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.BestAvailableEncryption(str.encode(key_passphrase)),
+        ))
+
+    subject = issuer = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, "CA"),
+    ])
+
+    # generate CA cert
+    ca_cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        ca_key.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.now(datetime.timezone.utc)
+    ).not_valid_after(
+        datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=2)
+    ).add_extension(
+        x509.BasicConstraints(ca=True, path_length=None),
+        critical=True,
+    ).add_extension(
+        x509.KeyUsage(
+            digital_signature=True,
+            content_commitment=False,
+            key_encipherment=False,
+            data_encipherment=False,
+            key_agreement=False,
+            key_cert_sign=True,
+            crl_sign=True,
+            encipher_only=False,
+            decipher_only=False,
+        ),
+        critical=True,
+    ).add_extension(
+        x509.SubjectKeyIdentifier.from_public_key(ca_key.public_key()),
+        critical=False,
+    ).sign(ca_key, hashes.SHA256())
+
+    # write CA cert to the file
+    with open(cert_path, "wb") as f:
+        f.write(ca_cert.public_bytes(serialization.Encoding.PEM))
+
+
+
+def generate_server_certificate(server_san: str, cert_path: str, key_path: str, key_passphrase: str, ca_cert_path: str, ca_key_path: str, ca_key_passphrase: str):
+
+    # load CA key
+    with open(ca_key_path, "rb") as f:
+        ca_key_data = f.read()
+    ca_key = load_pem_private_key(ca_key_data, str.encode(ca_key_passphrase))
+
+    # load CA cert
+    with open(ca_cert_path, "rb") as f:
+        ca_cert_data = f.read()
+    ca_cert = x509.load_pem_x509_certificate(ca_cert_data)
+
+    # generate server key
+    server_key = ec.generate_private_key(ec.SECP256R1())
+
+    # write server key to the file
+    with open(key_path, "wb") as f:
+        f.write(server_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.BestAvailableEncryption(str.encode(key_passphrase)),
+        ))
+
+    subject = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, server_san),
+    ])
+
+    # generate server cert
+    server_cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        ca_cert.subject
+    ).public_key(
+        server_key.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.now(datetime.timezone.utc)
+    ).not_valid_after(
+        datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=1)
+    ).add_extension(
+        x509.SubjectAlternativeName([
+            x509.DNSName(server_san),
+        ]),
+        critical=False,
+    ).add_extension(
+        x509.BasicConstraints(ca=False, path_length=None),
+        critical=True,
+    ).add_extension(
+        x509.KeyUsage(
+            digital_signature=True,
+            content_commitment=False,
+            key_encipherment=True,
+            data_encipherment=False,
+            key_agreement=False,
+            key_cert_sign=False,
+            crl_sign=True,
+            encipher_only=False,
+            decipher_only=False,
+        ),
+        critical=True,
+    ).add_extension(
+        x509.ExtendedKeyUsage([
+            x509.ExtendedKeyUsageOID.SERVER_AUTH,
+        ]),
+        critical=False,
+    ).add_extension(
+        x509.SubjectKeyIdentifier.from_public_key(server_key.public_key()),
+        critical=False,
+    ).add_extension(
+        x509.AuthorityKeyIdentifier.from_issuer_subject_key_identifier(
+            ca_cert.extensions.get_extension_for_class(x509.SubjectKeyIdentifier).value
+        ),
+        critical=False,
+    ).sign(ca_key, hashes.SHA256())
+
+    # write server cert to the file
+    with open(cert_path, "wb") as f:
+        f.write(server_cert.public_bytes(serialization.Encoding.PEM))
+
+
+
+def generate_client_certificate(client_cn: str, cert_path: str, key_path: str, key_passphrase: bytes, ca_cert_path: str, ca_key_path: str, ca_key_passphrase: bytes):
+
+    # load CA key
+    with open(ca_key_path, "rb") as f:
+        ca_key_data = f.read()
+    ca_key = load_pem_private_key(ca_key_data, str.encode(ca_key_passphrase))
+
+    # load CA cert
+    with open(ca_cert_path, "rb") as f:
+        ca_cert_data = f.read()
+    ca_cert = x509.load_pem_x509_certificate(ca_cert_data)
+
+    # generate client key
+    client_key = ec.generate_private_key(ec.SECP256R1())
+
+    # write client key to the file
+    with open(key_path, "wb") as f:
+        f.write(client_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.BestAvailableEncryption(str.encode(key_passphrase)),
+        ))
+
+    subject = x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, client_cn),
+    ])
+
+    # generate client cert
+    client_cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        ca_cert.subject
+    ).public_key(
+        client_key.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.now(datetime.timezone.utc)
+    ).not_valid_after(
+        datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=1)
+    ).add_extension(
+        x509.BasicConstraints(ca=False, path_length=None),
+        critical=True,
+    ).add_extension(
+        x509.KeyUsage(
+            digital_signature=True,
+            content_commitment=False,
+            key_encipherment=True,
+            data_encipherment=False,
+            key_agreement=False,
+            key_cert_sign=False,
+            crl_sign=True,
+            encipher_only=False,
+            decipher_only=False,
+        ),
+        critical=True,
+    ).add_extension(
+        x509.ExtendedKeyUsage([
+            x509.ExtendedKeyUsageOID.CLIENT_AUTH,
+        ]),
+        critical=False,
+    ).add_extension(
+        x509.SubjectKeyIdentifier.from_public_key(client_key.public_key()),
+        critical=False,
+    ).add_extension(
+        x509.AuthorityKeyIdentifier.from_issuer_subject_key_identifier(
+            ca_cert.extensions.get_extension_for_class(x509.SubjectKeyIdentifier).value
+        ),
+        critical=False,
+    ).sign(ca_key, hashes.SHA256())
+
+    # write client cert to the file
+    with open(cert_path, "wb") as f:
+        f.write(client_cert.public_bytes(serialization.Encoding.PEM))
