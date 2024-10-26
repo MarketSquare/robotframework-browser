46e2cf0cf2942f2af8f67886851e22676e321eb6
Tatu Aalto <aalto.tatu@gmail.com>
2024-10-27 02:36:07 +0300

    Lint fixes

--- Browser/gen_stub.py
+++ Browser/gen_stub.py
@@ -104,11 +104,24 @@ function_list = get_function_list_from_keywords(br.get_keyword_names())
 
 
 pyi_boilerplate = """\
+# Copyright 2020-     Robot Framework Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import datetime
 import typing
 import os
 from concurrent.futures import Future
-from datetime import datetime
 from os import PathLike
 from typing import Any
 
--- atest/test/01_Browser_Management/clock.robot
+++ atest/test/01_Browser_Management/clock.robot
@@ -1,8 +1,8 @@
 *** Settings ***
-Resource        imports.resource
+Resource            imports.resource
 
-Test Setup      Test Setup For Clock
-Suite Teardown    Suite Teardown For Clock
+Suite Teardown      Suite Teardown For Clock
+Test Setup          Test Setup For Clock
 
 *** Test Cases ***
 Set Time To Fixed
@@ -16,7 +16,6 @@ Set Time To System
     ${time_past_hour} =    Get Current Date    increment=-5 hours    result_format=%d.%m.%Y klo %H
     Clock Set Time    ${time_past}    system
     Get Text    id=current-time    starts    ${time_past_hour}
-
 
 *** Keywords ***
 Test Setup For Clock
