81cf9a1f94ba91689f08e92e815a291491d258c9
Tatu Aalto <2665023+aaltat@users.noreply.github.com>
2025-02-03 07:07:21 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -580,7 +580,7 @@ def _run_pabot(extra_args=None, shard=None, include_mac=False):
     process.wait(ATEST_TIMEOUT)
     output_xml = str(ATEST_OUTPUT / "output.xml")
     print(f"Process {output_xml}")
-    robotstatuschecker.process_output(output_xml, verbose=False)
+    robotstatuschecker.process_output(output_xml)
     rc = rebot_cli(["--outputdir", str(ATEST_OUTPUT), output_xml], exit=False)
     print(f"DONE rc=({rc})")
     return rc
