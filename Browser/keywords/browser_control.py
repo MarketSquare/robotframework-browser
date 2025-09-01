b547761336c71a2fdab9984d7056e2b90aee53ae
René <snooz@posteo.de>
2025-09-01 20:25:36 +0200

    first impl
    Signed-off-by: René <snooz@posteo.de>

--- Browser/keywords/browser_control.py
+++ Browser/keywords/browser_control.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import base64
+from enum import Enum
 import json
 import uuid
 from collections.abc import Iterable
@@ -22,6 +23,8 @@ from typing import TYPE_CHECKING, ClassVar, Optional, Union
 
 from robot.libraries.BuiltIn import BuiltIn, RobotNotRunningError
 from robot.utils import get_link_path
+
+from Browser.utils.data_types import InstallableBrowser, InstallationOptions
 
 from ..base import LibraryComponent
 from ..generated.playwright_pb2 import Request
@@ -633,3 +636,21 @@ class Control(LibraryComponent):
         with self.playwright.grpc_channel() as stub:
             response = stub.ClearPermissions(Request().Empty())
             logger.info(response.log)
+
+    @keyword
+    def install_browser(self, browser: Optional[InstallableBrowser] = None, *options: InstallationOptions):
+        """Executes a Playwright command with the given arguments."""
+        with self.playwright.grpc_channel() as stub:
+            try:
+                response = stub.ExecutePlaywright(
+                    Request().Json(
+                        body=json.dumps(['install']
+                            + [opt.value for opt in options]
+                            + ([browser.value] if browser else [])
+                        )
+                    )
+                )
+                logger.info(response.log)
+            except Exception:
+                logger.error("Error executing Playwright command")
+
