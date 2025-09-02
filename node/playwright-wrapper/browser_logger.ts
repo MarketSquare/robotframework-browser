4cfc38db59ed78240731ea3d2387cf4fc05ee375
Tatu Aalto <aalto.tatu@gmail.com>
2025-09-02 22:28:47 +0300

    Make pino lgger level configurable again

--- node/playwright-wrapper/browser_logger.ts
+++ node/playwright-wrapper/browser_logger.ts
@@ -28,4 +28,4 @@
 
 import { pino, stdTimeFunctions } from 'pino';
 
-export const logger = pino({ timestamp: stdTimeFunctions.isoTime });
+export const logger = pino({ timestamp: stdTimeFunctions.isoTime, level: process.env.PINO_LOG_LEVEL || 'info'});
