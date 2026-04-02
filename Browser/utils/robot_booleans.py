353229d8cfb18b53b70bb7e2d6fb1ace92aa5fbc
Tatu Aalto <aalto.tatu@gmail.com>
2026-04-02 19:58:43 +0300

    chore: use is_truthy and is_falsy from RF directly

--- Browser/utils/robot_booleans.py
+++ Browser/utils/robot_booleans.py
@@ -11,20 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from typing import Any
-
-TRUE_STRINGS = {"TRUE", "YES", "ON", "1", "CHECKED"}
-FALSE_STRINGS = {"FALSE", "NO", "OFF", "0", "UNCHECKED", "NONE", ""}
-
-
-def is_truthy(item: Any) -> bool:
-    if isinstance(item, bool):
-        return item
-    if isinstance(item, str):
-        return item.upper() not in FALSE_STRINGS
-    return bool(item)
-
-
-def is_falsy(item: Any) -> bool:
-    return item is None or not is_truthy(item)
+from robot.utils.robottypes import is_falsy as is_falsy  # noqa: PLC0414
+from robot.utils.robottypes import is_truthy as is_truthy  # noqa: PLC0414
